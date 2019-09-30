9a7d91d5a1f63653eb6bec01c914a1e46c84c6e9
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-09-30 14:24:40 +0200

    Add copy-icons script.

---
+++ copy-icons.js
@@ -0,0 +1,20 @@
+var fs = require("fs");
+var fse = require("fs-extra");
+var path = require("path");
+
+var currentDirPath = path.resolve();
+var basePath = currentDirPath.concat("/../../assets/icons/");
+// check if assets folder exists
+!fs.existsSync(basePath) && fs.mkdirSync(basePath);
+if (fs.existsSync(basePath)) {
+  var currentPath = path.resolve();
+  fs.readdir(currentPath, (err, files) => {
+    files.forEach(file => {
+      // double check if file is file
+      if (fs.lstatSync(file).isFile() && path.extname(file) == ".png") {
+        var destFile = basePath.concat(file);
+        fse.copy(path.resolve(__dirname,file), destFile);
+      }
+    });
+  });
+}
